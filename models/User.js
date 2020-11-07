e5f210e0b4b60bad750864e846b8dc46f7408506
Jordan Roberts <jdrobs19@gmail.com>
2020-11-06 20:12:00 -0700

    started User model and routes

--- models/User.js
+++ models/User.js
@@ -1,18 +1,40 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
+const bcrypt = require('bcrypt');
 
 class User extends Model {}
 
 User.init(
     {
-        name: {
-            type: DataTypes.STRING
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
         }
     },
     {
         sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
         modelName: 'user'
     }
-);
+)
 
 module.exports = User;
--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-require('dotenv').config();
-const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
+const routes = require('./controllers');
+const sequelize = require('./config/config');
+const path = require('path');
 const exphbs = require('express-handlebars');
-const PORT = process.env.PORT || 3006;
+const morgan = require('morgan');
 
 const app = express();
-const sequelize = require('./config/config');
+const PORT = process.env.PORT || 3006;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(morgan('dev'));
-app.use(require('./routes/htmlRoutes'));
+app.use(routes);
 
-sequelize.sync({force: false}).then(() => {
-    app.listen(PORT, ()=> {
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
         console.log(`app now running on http://localhost:${PORT}/`);
     })
-})
+});
