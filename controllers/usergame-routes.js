1b0022c392beeb339747ddbb3971be81f1898e06
Jordan Roberts <jdrobs19@gmail.com>
2020-11-12 19:59:32 -0700

    started user display route on games

--- controllers/usergame-routes.js
+++ controllers/usergame-routes.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
-const {Games} = require ('../models');
+const {User , Games} = require ('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/:id', withAuth, (req,res) => {
-    res.render('usergame', {layout: 'game'});
+router.get('/:id', (req,res) => {
+    User.findAll({
+        where: {
+            game_id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        const users = dbUserData.map((user) => user.get({plain: true}));
+        res.render('usergame', {layout: 'game'});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 module.exports = router;
